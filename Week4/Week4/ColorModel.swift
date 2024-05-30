10428d5a2b52f8e745e2850df3162dfaf09f4944
rania <raniaarbash@hotmail.com>
2024-05-30 09:10:31 -0400

    Week 4 - Color App

---
+++ Week4/Week4/ColorModel.swift
@@ -0,0 +1,23 @@
+//
+//  ColorModel.swift
+//  Week4
+//
+//  Created by Rania Arbash on 2024-05-30.
+//
+
+import Foundation
+import UIKit
+
+
+class ColorModel {
+    
+    var colorName: String = ""
+    var color : UIColor = UIColor.red
+    
+    
+    init(n: String, c: UIColor) {
+        colorName = n
+        color = c
+    }
+    
+}
---
+++ Week4/Week4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week4
+//
+//  Created by Rania Arbash on 2024-05-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
